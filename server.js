31fdedf095a4ccb76197fafa6f9c77787b861593
Filip Stankovic <filips@gmail.com>
2016-04-12 12:45:31 -0400

    Setup mongoose with blog model and test data

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-var express = require('express');
+var express = require('express');;
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/blogroll-tutorial');
+
+var Schema = mongoose.Schema;
+
+var BlogSchema = new Schema({
+  author: String,
+  title: String,
+  url: String
+});
+
+mongoose.model('Blog', BlogSchema);
+
+var Blog = mongoose.model('Blog');
+
+var blog = new Blog({
+  author: "test",
+  title: "test title",
+  url: "http://test.com"
+})
+
+blog.save();
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
